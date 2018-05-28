ea72da215dcf70de5ca6f8d9b74c36b530680fac
Mohamed Romdhani <mromdhani7@gmail.com>
2018-05-28 23:09:26 +0100

    Commit Initial - Upgraded to Angular 6 + Bootstrap 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { ProductsListComponent } from './components/products-list/products-list.component';
+import { ProductsFilterPipe } from './pipes/products-filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsListComponent,
+    ProductsFilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
